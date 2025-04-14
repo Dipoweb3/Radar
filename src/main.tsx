b0e8c72a20f641639f32074eea7251b2f92fbd51
Adefila Muyeez <muyeezadefila@gmail.com>
2025-04-14 05:08:19 -0700

    Add ToastContext and wrap App in provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { ToastProvider } from './context/ToastContext'; // Adjust path if needed
 
 createRoot(document.getElementById("root")!).render(
-  <ToastProvider>
-    <App />
-  </ToastProvider>
+  <App />
 );
